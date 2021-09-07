2cd0dcffa0df580673c238d74a0d28a1eeeae65f
starrynight86 <87331711+starrynight86@users.noreply.github.com>
2021-09-07 20:00:51 +0200

    Fixed Forecast
    Injected real life data in the forecast section

--- src/script.js
+++ src/script.js
@@ -96,25 +96,42 @@ let day = days[now.getDay()];
 let actualDate = document.querySelector("#actual-date");
 actualDate.innerHTML = `${day} ${hours}:${minutesFormatted}`;
 
-function displayForecast() {
-	let days = ["Mon", "Tue", "Wed", "Thu"];
+function formatDay(timestamp) {
+	let date = new Date(timestamp * 1000);
+	let day = date.getDay();
+	let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+	return days[day];
+}
+
+function displayForecast(response) {
+	let forecast = response.data.daily;
 	let forecastElement = document.querySelector("#forecast");
 	let forecastHTML = `<div class="row">`;
 
-	days.forEach(function (day) {
-		forecastHTML =
-			forecastHTML +
-			`<div class="col-2">
-								<div class="weather-forecast-date"><h6>${day}</h6></div>
-
-								<i class="fas fa-cloud-sun-rain meteo-icon"></i>
+	forecast.forEach(function (forecastDay, index) {
+		if (index > 0 && index < 7) {
+			forecastHTML =
+				forecastHTML +
+				`<div class="col-2">
+								<div class="weather-forecast-date"><h6 class="forecast-day">${formatDay(
+									forecastDay.dt
+								)}</h6></div>
+								<img src = "http://openweathermap.org/img/wn/${
+									forecastDay.weather[0].icon
+								}@2x.png" alt ="" width="42"
+								/>
 								<br />
 
 								<div class="weather-forecast-temperatures">
-									<span class="weather-forecast-temperature-max"> 21°c</span>
-									<span class="weather-forecast-temperature-min">15°c</span>
+									<span class="weather-forecast-temperature-max"> ${Math.round(
+										forecastDay.temp.max
+									)}°C |</span>
+									<span class="weather-forecast-temperature-min"> ${Math.round(
+										forecastDay.temp.min
+									)}°C</span>
 								</div>
 							</div>`;
+		}
 	});
 
 	forecastHTML = forecastHTML + `</div>`;
